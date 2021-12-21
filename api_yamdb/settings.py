dfe8c95e3eeb2584d662dcf261da900e63953d99
Антон Кобелев <anton8963kobelev@yandex.ru>
2021-12-21 13:15:08 +0400

    install python-decouple

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,9 +1,10 @@
 import os
 from datetime import timedelta
+from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
